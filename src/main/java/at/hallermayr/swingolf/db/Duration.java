996d76f452d66145e52e5edb6b5c92bb346ba3ac
Rudolf Traunmüller <rudolf.traunmueller@maat.at>
2018-01-28 08:26:36 +0100

    add duration entity

---
+++ src/main/java/at/hallermayr/swingolf/db/Duration.java
@@ -0,0 +1,43 @@
+package at.hallermayr.swingolf.db;
+
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Relationship;
+
+@NodeEntity
+public class Duration {
+
+	@GraphId private Long id;
+
+	private String from;
+
+    private String to;
+
+	protected Duration() {
+		// Empty constructor required as of Neo4j API 2.0.5
+	};
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+}
